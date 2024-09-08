c4ffb0aa9ef7d31b983c7dd57bb16832c92ae307
bohan_huang <demonfang16@gmail.com>
2024-09-07 18:12:32 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -9,64 +9,6 @@
  * "I was pressed!" and nothing.
 /** */
 double IntakeTempSpeed = 0;
-static void MoveIntake() {
-    int intakespeed;
-    int intakemovespeed;
-    if (master.get_digital_new_press(DIGITAL_L1)){
-      intake.move(126);
-    }
-    else if (master.get_digital_new_press(DIGITAL_L2))
-    {
-      intake.move(-126);
-    }
-    else if (master.get_digital_new_press(DIGITAL_X))
-    {
-      intake.move(0);
-    }
-    else{
-      intake.move(0);
-    }
-    pros::delay(20);
-}
-    /**if(master.get_digital_new_press(DIGITAL_L1)) {
-        if (IntakeTempSpeed==1){
-          IntakeTempSpeed = 0;
-          intakespeed = 99;
-        }      
-        else if(IntakeTempSpeed == 0){
-          IntakeTempSpeed = 1;
-        }
-        else{
-          IntakeTempSpeed = 1;
-        }
-
-    if (IntakeTempSpeed == 1){
-      intakespeed = 99;
-    }
-    else if (IntakeTempSpeed ==0){
-      intakespeed = 35; 
-    }
-    else {
-      intakespeed = 99;
-    }
-    
-    if (master.get_digital_new_press(DIGITAL_L2)){
-      intakemovespeed = 126;
-
-    }
-    if (master.get_digital_new_press(DIGITAL_L1)){
-      intakemovespeed = -126;
-
-    }
-    if (master.get_digital_new_press(DIGITAL_X)){
-      intakemovespeed = 0;
-
-    }
-    intake.move(intakemovespeed);
-    pros::delay(20);
-    }
-}
-*/
 
 
 void turn(double heading) {
@@ -109,76 +51,67 @@ void turn(double heading) {
 
 
 
-
 void drivePID(){
-	double kP=3.1;
-double kI =1;
-double kD =1;
-int desiredposition;
-	//Settings double kP double ki double kD
-int error; //Sensorvalue
-int prevError;  //Position 20 wiliseconds ago
-int derivitave;
-int totalerror;
-bool enabledrivePID = true;
-	while (enabledrivePID)
-	{
-		int leftdrivevalue = left_mg.get_position();
-		int rightdrivevalue = right_mg.get_position();
-		int avgposition = (left_mg.get_position()+right_mg.get_position())/2;
-		error=desiredposition-avgposition;
-		derivitave = error - prevError;
-
-		totalerror+=error;
-
-
-		double motorpower= (error*kP + totalerror * kI + derivitave * kD);
-		left_mg.move_velocity(motorpower);
-		right_mg.move_velocity(motorpower);
-
-
-		prevError-error;
-		pros::delay(20);
-		
-	}
-	left_mg.move_velocity(0);
-		right_mg.move_velocity(0);
-	
+    double kP=3.1;
+    double kI =1;
+    double kD =1;
+    int desiredposition;
+    //Settings double kP double ki double kD
+    int error; //Sensorvalue
+    int prevError;  //Position 20 wiliseconds ago
+    int derivitave;
+    int totalerror;
+    bool enabledrivePID = true;
+    while (enabledrivePID) {
+        int leftdrivevalue = left_mg.get_position();
+        int rightdrivevalue = right_mg.get_position();
+        int avgposition = (left_mg.get_position()+right_mg.get_position())/2;
+        error=desiredposition-avgposition;
+        derivitave = error - prevError;
+        totalerror+=error;
+        double motorpower= (error*kP + totalerror * kI + derivitave * kD);
+        left_mg.move_velocity(motorpower);
+        right_mg.move_velocity(motorpower);
+        prevError-=error;
+        pros::delay(20);
+    }
+    left_mg.move_velocity(0);
+    right_mg.move_velocity(0);
 }
 
 void resetdriveEncoders(){
-  left_mg.tare_position();
-  right_mg.tare_position();
+    left_mg.tare_position();
+    right_mg.tare_position();
 }
 
 
 void firstroute(){
     if (master.get_digital_new_press(DIGITAL_DOWN)){
-      intake.move(126);
-      left_mg.move_relative(7020,126);
-      right_mg.move_relative(7020,126);
-      left_mg.move_relative(-360,126);
-      right_mg.move_relative(360,126);
-      /*left_mg.move_relative(10530,126);
-      right_mg.move_relative(10530,126);
-       left_mg.move_relative(45,126);
+        intake.move(126);
+        left_mg.move_relative(7020,126);
+        right_mg.move_relative(7020,126);
+        left_mg.move_relative(-360,126);
+        right_mg.move_relative(360,126);
+        left_mg.move_relative(10530,126);
+        right_mg.move_relative(10530,126);
+        left_mg.move_relative(45,126);
         right_mg.move_relative(-45,126);
         left_mg.move_relative(7847.775,126);
         left_mg.move_relative(7847.775,126);
         left_mg.move_relative(-45,126);
         right_mg.move_relative(45,126);
         left_mg.move_relative(7020,126);
-      right_mg.move_relative(7020,126);
-      left_mg.move_relative(-135,126);
+        right_mg.move_relative(7020,126);
+        left_mg.move_relative(-135,126);
         right_mg.move_relative(135,126);
         left_mg.move_relative(7847.775,126);
         left_mg.move_relative(7847.775,126);
         left_mg.move_relative(-45,126);
         right_mg.move_relative(45,126);
         left_mg.move_relative(2*7847.775,126);
-      left_mg.move_relative(2*7847.775,126);
-pros::delay(20);
-      /*24,turn 45, move 26.83,move 24. turn 135, move 26.83, 53.665*/}
+        left_mg.move_relative(2*7847.775,126);
+        pros::delay(20);
+        /*24,turn 45, move 26.83,move 24. turn 135, move 26.83, 53.665*/}
 }
 
 /**
@@ -188,7 +121,7 @@ pros::delay(20);
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	
+  
 }
 
 /**
@@ -237,112 +170,101 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+    // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
+    while (true) {
+        if (master.get_digital(DIGITAL_L1)){
+            IntakeTempSpeed = 1;
+        }
+        else if (master.get_digital(DIGITAL_L2)){
+            IntakeTempSpeed = 2;
+        }
+        else if (master.get_digital(DIGITAL_X)){
+            IntakeTempSpeed = 0;
+        }
+        else {
+            IntakeTempSpeed = 0;
+        }
 
-	while (true) {
-		
-    if (master.get_digital(DIGITAL_L1)){
-      IntakeTempSpeed = 1;
-      
-    }
-    else if (master.get_digital(DIGITAL_L2))
-    {
-      IntakeTempSpeed = 2;
-    }
-    else if (master.get_digital(DIGITAL_X))
-    {
-      IntakeTempSpeed = 0;
-    }
-    else{
-      IntakeTempSpeed = 0;
-    }
-   if (IntakeTempSpeed==1){
-      intake.move(126);
-      pros::delay(20);
-   }
-    if (IntakeTempSpeed==2){
-      intake.move(-126);
-      pros::delay(20);
-   }
-if (IntakeTempSpeed==0){
-      intake.move(0);
-      pros::delay(20);
-   }
-		if (master.get_digital_new_press(DIGITAL_A)) {
-			left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
-			right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+        if (IntakeTempSpeed==1){
+            intake.move(126);
+            pros::delay(20);
+        }
+        if (IntakeTempSpeed==2){
+            intake.move(-126);
+            pros::delay(20);
+        }
+        if (IntakeTempSpeed==0){
+            intake.move(0);
+            pros::delay(20);
+        }
+
+        if (master.get_digital_new_press(DIGITAL_A)) {
+            left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+            right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
             master.print(0, 0, "                             ");
-            
+                
             double start = pros::millis();
             left_mg.move_relative(7020, 126);
-			right_mg.move_relative(7020, 126);
-			pros::delay(20);
-
+            right_mg.move_relative(7020, 126);
+            pros::delay(20);
 
             master.print(0, 0, "%.0f                             ", (pros::millis() - start));
             pros::delay(500);
             master.print(1, 0, "%.2f                             ", inertial.get_heading());
             pros::delay(500);
-		}
-if (master.get_digital_new_press(DIGITAL_B)) {
-			left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
-			right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+        }
+        if (master.get_digital_new_press(DIGITAL_B)) {
+            left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+            right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
             master.print(0, 0, "                             ");
-            
+                
             double start = pros::millis();
             left_mg.move_relative(180, 126);
-			right_mg.move_relative(-180, 126);
-			pros::delay(20);
-
+            right_mg.move_relative(-180, 126);
+            pros::delay(20);
 
             master.print(0, 0, "%.0f                             ", (pros::millis() - start));
             pros::delay(500);
             master.print(1, 0, "%.2f                             ", inertial.get_heading());
             pros::delay(500);
-		}
-     if (master.get_digital_new_press(DIGITAL_DOWN)){
-      
-     left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
-			right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+        }
+        if (master.get_digital_new_press(DIGITAL_DOWN)){
+            left_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
+            right_mg.set_brake_mode_all(MOTOR_BRAKE_HOLD);
             master.print(0, 0, "                             ");
-            
+                
             double start = pros::millis();
             left_mg.move_relative(10530,126);
-      right_mg.move_relative(10530,126);
-       left_mg.move_relative(45,126);
-        right_mg.move_relative(-45,126);
-        left_mg.move_relative(7847.775,126);
-        left_mg.move_relative(7847.775,126);
-        left_mg.move_relative(-45,126);
-        right_mg.move_relative(45,126);
-        left_mg.move_relative(7020,126);
-      right_mg.move_relative(7020,126);
-      left_mg.move_relative(-135,126);
-        right_mg.move_relative(135,126);
-        left_mg.move_relative(7847.775,126);
-        left_mg.move_relative(7847.775,126);
-        left_mg.move_relative(-45,126);
-        right_mg.move_relative(45,126);
-        left_mg.move_relative(2*7847.775,126);
-      left_mg.move_relative(2*7847.775,126);
-pros::delay(20);
-			pros::delay(20);
-
-
+            right_mg.move_relative(10530,126);
+            left_mg.move_relative(45,126);
+            right_mg.move_relative(-45,126);
+            left_mg.move_relative(7847.775,126);
+            left_mg.move_relative(7847.775,126);
+            left_mg.move_relative(-45,126);
+            right_mg.move_relative(45,126);
+            left_mg.move_relative(7020,126);
+            right_mg.move_relative(7020,126);
+            left_mg.move_relative(-135,126);
+            right_mg.move_relative(135,126);
+            left_mg.move_relative(7847.775,126);
+            left_mg.move_relative(7847.775,126);
+            left_mg.move_relative(-45,126);
+            right_mg.move_relative(45,126);
+            left_mg.move_relative(2*7847.775,126);
+            left_mg.move_relative(2*7847.775,126);
+            pros::delay(40);
             master.print(0, 0, "%.0f                             ", (pros::millis() - start));
             pros::delay(500);
             master.print(1, 0, "%.2f                             ", inertial.get_heading());
             pros::delay(500);
 
-     }
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir + turn);                      // Sets left motor voltage
-		right_mg.move(dir - turn);                     // Sets right motor voltage
-		pros::delay(20);                             // Run for 20 ms then update
-	}
-
-
-
+        }
+        
+        int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+        int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+        left_mg.move(dir + turn);                      // Sets left motor voltage
+        right_mg.move(dir - turn);                     // Sets right motor voltage
+        pros::delay(20);                             // Run for 20 ms then update
+    }
 }
